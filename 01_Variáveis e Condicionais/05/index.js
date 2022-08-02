4e59ce2a637547f5a33908840b54723233577ad6
unknown <victorh.reinaldo@gmail.com>
2022-08-01 21:27:45 -0300

    Resolução-variáveis e condicionais

---
+++ 01_Variáveis e Condicionais/05/index.js
@@ -0,0 +1,6 @@
+const posicaoA = 27;
+const posicaoB = 14;
+const distancia =
+  posicaoA > posicaoB ? posicaoA - posicaoB : posicaoB - posicaoA;
+
+console.log(`Adistância entre os carros é de ${distancia} km`);
