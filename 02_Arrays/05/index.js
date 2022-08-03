9bcb8c61794b9b8544562cdf873218ee3798916d
unknown <victorh.reinaldo@gmail.com>
2022-08-03 14:30:43 -0300

    Resolução - IBGE OTIMIZADO

--- 02_Arrays/05/index.js
+++ 02_Arrays/05/index.js
@@ -7,47 +7,35 @@ const dados = [
 function mediaSalarial(infos) {
   let somaA = [];
   let totalA = 0;
-  let itensA = 0;
   let somaJ = [];
   let totalJ = 0;
-  let itensJ = 0;
   let somaI = [];
   let totalI = 0;
-  let itensI = 0;
   let arraySomas = [];
   for (let item of infos) {
     if (item[0] === "A") {
       somaA = item.slice(1);
       for (let i of somaA) {
         totalA += i;
-        itensA++;
       }
       arraySomas.push(totalA / somaA.length);
-      break;
     }
-  }
-  for (let item of infos) {
     if (item[0] === "J") {
       somaJ = item.slice(1);
       for (let i of somaJ) {
         totalJ += i;
-        itensJ++;
       }
       arraySomas.push(totalJ / somaJ.length);
-      break;
     }
-  }
-  for (let item of infos) {
     if (item[0] === "I") {
       somaI = item.slice(1);
       for (let i of somaI) {
         totalI += i;
-        itensI++;
       }
       arraySomas.push(totalI / somaI.length);
-      break;
     }
   }
+
   if (Math.max(...arraySomas) === arraySomas[0]) {
     return `A maior média salarial é a do Adulto com R$ ${arraySomas[0].toFixed(
       2
